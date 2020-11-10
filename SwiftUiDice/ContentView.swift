c43cbb71827b9b467f439fc4b09c77c6bb15b311
ckk16a <ckk16a@acu.edu>
2020-11-10 08:20:20 -0600

    Initial Commit

---
+++ SwiftUiDice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiDice
+//
+//  Created by Carson Kelley on 11/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
