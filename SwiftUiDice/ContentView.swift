112d2aa504a9c93f7a485f3f10929898cb040731
ckk16a <ckk16a@acu.edu>
2020-11-10 09:34:12 -0600

    Finished Thing

--- SwiftUiDice/ContentView.swift
+++ SwiftUiDice/ContentView.swift
@@ -6,11 +6,53 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+//    @State var diceImage = "d1"
+//    @State var diceText = "Tap the dice or shake to roll!"
+    
+    @State var rolledNumber = 1
+    @State var diceText = "Tap the dice or shake to roll!"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            ShakableViewRepresentable()
+                .allowsHitTesting(false)
+            VStack(alignment: .center) {
+                Button(action: {
+                    rollDice()
+                }) {
+                    Image("d\(rolledNumber)")
+                        .padding(10)
+                }
+                Text(diceText)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.black)
+                    .padding(10)
+            }
+                .onReceive(messagePublisher) { _ in
+                            rollDice()
+            }
+        }
+    }
+    
+    func rollDice() {
+        let randomNumber = Int.random(in: 1...20)
+
+        self.rolledNumber = randomNumber
+
+        if(randomNumber == 1){
+            self.diceText = "Critical Miss! #Oof"
+            playSound(sound: "criticalmiss", type: "mp3")
+        } else if(randomNumber == 20){
+            self.diceText = "Critical Hit! It's Your Lucky Day!"
+            playSound(sound: "criticalhit", type: "mp3")
+        } else{
+            playSound(sound: "diceroll", type: "wav")
+            self.diceText = ""
+        }
+        
     }
 }
 
